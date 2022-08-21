6e2cc8bfa0d44ac005d0b2a8b01ae6a75e6d1719
sharif <sharif.jatri@gmail.com>
2022-08-21 18:01:58 +0600

    deletepost api connected

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+<!--  <label>https://www.youtube.com/watch?v=UbYt1PokMrM</label>-->
   <ul>
     <li v-for="post in allPosts" :key="post.id">
         <h4>{{ post.id }}</h4>
         <h4>{{ post.title }}</h4>
-      <button>Delete</button>
+      <button @click="deleteAndUpdateCache(post.id)">Delete</button>
     </li>
   </ul>
 </template>
 
 <script>
 
-import {useQuery} from '@vue/apollo-composable'
+import {useQuery, useMutation} from '@vue/apollo-composable'
 import allPostsQuery from '../src/graphql/posts/allPosts.query.gql'
+import deletePostMutation from '../src/graphql/posts/deletePost.mutation.gql'
+
 import {computed} from "vue";
 
 export default {
@@ -22,8 +25,22 @@ export default {
     const {result} = useQuery(allPostsQuery)
     const allPosts = computed(() => result.value?.posts.data ?? []);
 
+    const { mutate: deletePost } = useMutation(deletePostMutation)
+
+    function deleteAndUpdateCache(id){
+      deletePost({ id }, {
+        update: (store, {}) => {
+          const data = store.readQuery({query: allPostsQuery})
+          const updateData = data.posts.data.filter(post => post.id !== id)
+          store.writeQuery({query: allPostsQuery, data: { data: updateData }})
+        }
+      }
+      )}
+
     return {
-      allPosts
+      allPosts,
+      deletePost,
+      deleteAndUpdateCache
     }
   },
 
