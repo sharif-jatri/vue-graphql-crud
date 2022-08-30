348b1d26c40fbb72a87535a91ba18c49f63cc035
sharif <sharif.jatri@gmail.com>
2022-08-30 16:15:55 +0600

    add mutation

--- src/components/JsonServerList.vue
+++ src/components/JsonServerList.vue
@@ -11,19 +11,37 @@
       <h2>Query Results</h2>
       <div v-for="p in result.allPosts" :key="p.id">{{p.title}}</div>
     </div>
+    <div>
+      <input type="text" v-model="title" placeholder="enter the title" />
+      <button @click="addPost">ADD POST</button>
+    </div>
   </div>
 </template>
 
 <script>
 import { gql } from "apollo-boost";
-import { useQuery } from "@vue/apollo-composable";
+import { useQuery, useMutation } from "@vue/apollo-composable";
+import {ref} from "vue";
+
 
 // QUERY
 const ALL_POST_QUERY = gql`
   {
     allPosts {
       id
-      title
+      title,
+      user_id
+    }
+  }
+`;
+
+// MUTATION
+const ADD_POST_MUTATION = gql`
+  mutation createPost($title: String!, $id: ID!, $userId: ID!) {
+    createPost(title: $title, views: 0, user_id: $userId, id: $id) {
+      id
+      title,
+      user_id
     }
   }
 `;
@@ -31,15 +49,41 @@ const ALL_POST_QUERY = gql`
 export default {
   name: 'JsonServerList',
   setup() {
+    const title = ref('')
+    const id = ref(200)
+    const userId = ref(200)
     // QUERY
     const { result, loading, error } = useQuery(
         ALL_POST_QUERY
     );
 
+    // MUTATION <== NEW
+    const {
+      loading: mLoading,
+      error: mError,
+      mutate: createPost
+    } = useMutation(ADD_POST_MUTATION, {
+      update: (cache, { data: { createPost } }) => {
+        // get the posts from the cache...
+        const data = cache.readQuery({ query: ALL_POST_QUERY });
+        // add the new post to the cache
+        data.allPosts.push(createPost);
+        // write results back to cache
+        cache.writeQuery({ query: ALL_POST_QUERY, data });
+      }
+    });
+
+    const addPost = () => {
+      createPost({title: title.value, id: id.value, userId: userId.value})
+    }
+
     return {
       result,
-      loading,
-      error
+      loading: loading || mLoading,
+      error: error || mError,
+      createPost,
+      addPost,
+      title
     };
   },
 }
